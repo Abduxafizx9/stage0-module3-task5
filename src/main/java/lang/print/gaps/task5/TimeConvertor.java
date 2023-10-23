2e6a0b2bbd0d8073cef7a0ce0e7d4ed37be675cb
Abdulxafizx9 <abdulxafizx999@gmail.com>
2023-10-23 22:22:16 +0500

    1.0

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        //minutes = 15;
+        float seconds = minutes*60;
+        System.out.println(seconds + "in seconds");
     }
 }
